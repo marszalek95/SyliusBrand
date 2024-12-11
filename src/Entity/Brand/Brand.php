7c8fd2614e10294d5c335b65b502bcf260940e82
marszalek95 <marszal1995@gmail.com>
2024-12-11 11:12:41 +0100

    added Brand entity

---
+++ src/Entity/Brand/Brand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity\Brand;
+
+use App\Repository\Brand\BrandRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+#[ORM\Entity(repositoryClass: BrandRepository::class)]
+class Brand implements ResourceInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
---
+++ src/Repository/Brand/BrandRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repository\Brand;
+
+use App\Entity\Brand\Brand;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+
+/**
+ * @extends ServiceEntityRepository<Brand>
+ */
+class BrandRepository extends EntityRepository
+{
+    
+}
