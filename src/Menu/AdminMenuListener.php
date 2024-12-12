cb6868455b22421793fa6b10cee24a64fa28c860
marszalek95 <marszal1995@gmail.com>
2024-12-12 11:34:30 +0100

    brand entity without image added

---
+++ src/Menu/AdminMenuListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMenuListener
+{
+    public function addAdminMenuItems(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $brandMenu = $menu
+            ->getChild('catalog')
+        ;
+
+        $brandMenu
+            ->addChild('new-subitem', [
+                'route' => 'app_admin_brand_index',
+            ])
+            ->setLabel('Brands')
+            ->setLabelAttribute('icon', 'tags')
+        ;
+    }
+}
