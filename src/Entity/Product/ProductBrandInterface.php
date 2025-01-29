5527436c3331dd9526981b2f7b0045e2e4f10ecd
marszalek95 <marszal1995@gmail.com>
2025-01-29 09:16:32 +0100

    some fix

--- src/Entity/Product/ProductBrandInterface.php
+++ src/Entity/Product/ProductBrandInterface.php
@@ -3,7 +3,6 @@
 namespace App\Entity\Product;
 
 use Sylius\Component\Product\Model\ProductInterface;
-use App\Entity\Brand\Brand;
 use App\Entity\Brand\BrandInterface;
 use Sylius\Component\Resource\Model\ResourceInterface;
 
